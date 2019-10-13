f01034a8fb9b8d569dcb836db8b4fb6189726de0
root <richardvu.work@gmail.com>
2019-10-13 21:37:28 +0700

    Create Java Core Structure

---
+++ java-core-structure/src/main/java/app/ws/ui/models/requests/Test.java
@@ -0,0 +1,19 @@
+/**
+ *Java Tutorial
+ * Java Tutorial - Coder's House Teams
+ * Coder's House Teams
+ * Skype: luongvu.work@gmail.com
+ * Email: codershouseteams@gmail.com
+ * Phone Number: (+84) 935710974
+ * hibernate-crud-with-mysql
+ * Oct 13, 2019
+ */
+package app.ws.ui.models.requests;
+
+/**
+ * @author richard
+ *
+ */
+public class Test {
+
+}
